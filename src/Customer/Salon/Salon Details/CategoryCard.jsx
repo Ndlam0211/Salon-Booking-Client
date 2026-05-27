59bf5378c8117055c721a9bad370c6049cf45894
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-05-27 21:05:19 +0700

    Create all services component

--- src/Customer/Salon/Salon Details/CategoryCard.jsx
+++ src/Customer/Salon/Salon Details/CategoryCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const CategoryCard = () => {
+const CategoryCard = ({handleCategoryClick, selectedCategory, item}) => {
   return (
-    <div>CategoryCard</div>
-  )
-}
+    <div onClick={handleCategoryClick}
+      className={`px-3 py-2 cursor-pointer flex gap-2 items-center ${selectedCategory === item ? "bg-green-500 text-white rounded-md":""}`}>
+      <img
+        className="w-14 h-14 object-cover rounded-full"
+        src="https://images.pexels.com/photos/28994568/pexels-photo-28994568/free-photo-of-styling-long-red-hair-with-flat-iron-indoors.jpeg?auto=compress&cs=tinysrgb&w=600"
+        alt=""
+      />
+      <h1>Hair Straightening</h1>
+    </div>
+  );
+};
 
-export default CategoryCard
+export default CategoryCard;
