7a758e2e7b759be216993cd9454b624c9c65f2fd
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-05-28 22:06:13 +0700

    Update tailwind config

---
+++ src/Salon/components/SalonDrawerList.jsx
@@ -0,0 +1,67 @@
+import { Dashboard } from '@mui/icons-material';
+import React from 'react'
+
+const menu = [
+  {
+    name: "Dashboard",
+    path: "/salon-dashboard",
+    icon: <Dashboard className='text-primary-color' />,
+    activeIcon: "",
+  },
+  {
+    name: "",
+    path: "",
+    icon: "",
+    activeIcon: "",
+  },
+  {
+    name: "",
+    path: "",
+    icon: "",
+    activeIcon: "",
+  },
+  {
+    name: "",
+    path: "",
+    icon: "",
+    activeIcon: "",
+  },
+  {
+    name: "",
+    path: "",
+    icon: "",
+    activeIcon: "",
+  },
+  {
+    name: "",
+    path: "",
+    icon: "",
+    activeIcon: "",
+  },
+  {
+    name: "",
+    path: "",
+    icon: "",
+    activeIcon: "",
+  },
+  {
+    name: "",
+    path: "",
+    icon: "",
+    activeIcon: "",
+  },
+  {
+    name: "",
+    path: "",
+    icon: "",
+    activeIcon: "",
+  },
+];
+
+const SalonDrawerList = () => {
+  return (
+    <div>SalonDrawerList</div>
+  )
+}
+
+export default SalonDrawerList
