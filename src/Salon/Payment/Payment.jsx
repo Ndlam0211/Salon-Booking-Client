95cd95390a45ede4f3cb8a4a83354127e6ecd744
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-06-05 12:22:38 +0700

    Implement Payment component to display total earnings and last payment details

--- src/Salon/Payment/Payment.jsx
+++ src/Salon/Payment/Payment.jsx
@@ -1,9 +1,21 @@
+import { Card, Divider } from '@mui/material'
 import React from 'react'
 
 const Payment = () => {
   return (
-    <div>Payment</div>
-  )
+    <div>
+      <div className="">
+        <Card className="rounded-md space-y-4 p-5">
+          <h1 className='text-gray-600 font-medium'>Total Earnings</h1>
+          <h1 className="font-bold text-xl pb-1">$1,500.00</h1>
+          <Divider />
+          <p>
+            Last Payment: <strong>$500.00</strong>
+          </p>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export default Payment
