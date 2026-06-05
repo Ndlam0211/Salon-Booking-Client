0a391513c71683ae0b8219ac9579ba4fe62dd2b4
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-06-05 10:38:40 +0700

    Enhance CategoryForm component with image upload and form handling; integrate Material-UI components for improved UI

--- src/Salon/Category/CategoryForm.jsx
+++ src/Salon/Category/CategoryForm.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import React from "react";
+import { AddPhotoAlternate, Close } from "@mui/icons-material";
+import CircularProgress from "@mui/material/CircularProgress";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useFormik } from "formik";
 
 const CategoryForm = () => {
-  return (
-    <div>CategoryForm</div>
-  )
-}
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      image: ""
+    },
+    onSubmit: () => {
+      console.log("submit: ", formik.values);
+    },
+  });
 
-export default CategoryForm
+  return (
+    <div className="flex justify-center items-center">
+      <form onSubmit={formik.handleSubmit} className="space-y-4 w-full lg:w-1/2">
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12 }} className="w-24 h-24">
+            {false ? (
+              <div className="relative border">
+                <img
+                  className="w-24 h-24 object-cover"
+                  src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2Fsb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="Category"
+                />
+                <IconButton
+                  className=""
+                  color="error"
+                  size="small"
+                  sx={{ position: "absolute", top: 0, right: 0 }}
+                >
+                  <Close sx={{ fontSize: "1rem" }} />
+                </IconButton>
+              </div>
+            ) : (
+              <>
+                <input
+                  type="file"
+                  accept="image/*"
+                  id="fileInput"
+                  style={{ display: "none" }}
+                />
+                <label htmlFor="fileInput" className="relative">
+                  <span className="w-24 h-24 cursor-pointer flex items-center justify-center p-3 border rounded-md border-gray-400">
+                    <AddPhotoAlternate className="text-gray-700" />
+                  </span>
+                  {false && (
+                    <div className="absolute left-0 right-0 top-0 bottom-0 w-24 h-24 flex justify-center items-center">
+                      <CircularProgress />
+                    </div>
+                  )}
+                </label>
+              </>
+            )}
+          </Grid>
+
+          <Grid size={12}>
+            <TextField
+              fullWidth
+              id="name"
+              name="name"
+              label="Category Name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              required
+            />
+          </Grid>
+          <Grid size={12}>
+            <Button
+              type="submit"
+              variant="outlined"
+              fullWidth
+              sx={{ py: ".8rem" }}
+            >
+              Create Category
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
+};
+
+export default CategoryForm;
