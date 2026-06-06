4f5ef0ac29116d8c9ffc9ec6c013f5f3126106ad
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-06-06 23:44:37 +0700

    Implement fetching of salon bookings in Payment component and calculate total earnings

--- src/Salon/Payment/Payment.jsx
+++ src/Salon/Payment/Payment.jsx
@@ -1,16 +1,38 @@
 import { Card, Divider } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getPriceTotal } from '../../util/totalEarnings';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSalonBookings } from '../../Redux/Booking/action';
 
 const Payment = () => {
+   const dispatch = useDispatch();
+   const { salon, booking } = useSelector((store) => store);
+
+   useEffect(() => {
+     if (salon.salon) {
+       dispatch(fetchSalonBookings(localStorage.getItem("jwt")));
+     }
+   }, [salon.salon]);
   return (
     <div>
       <div className="">
         <Card className="rounded-md space-y-4 p-5">
-          <h1 className='text-gray-600 font-medium'>Total Earnings</h1>
-          <h1 className="font-bold text-xl pb-1">$1,500.00</h1>
+          <h1 className="text-gray-600 font-medium">Total Earnings</h1>
+          <h1 className="font-bold text-xl pb-1">
+            {new Intl.NumberFormat("vi-VN", {
+              style: "currency",
+              currency: "VND",
+            }).format(getPriceTotal(booking.bookings))}
+          </h1>
           <Divider />
           <p>
-            Last Payment: <strong>$500.00</strong>
+            Last Payment:{" "}
+            <strong>
+              {new Intl.NumberFormat("vi-VN", {
+                style: "currency",
+                currency: "VND",
+              }).format(0)}
+            </strong>
           </p>
         </Card>
       </div>
