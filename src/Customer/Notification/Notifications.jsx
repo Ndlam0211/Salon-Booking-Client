5deb2121b46f0b736ba93f85cf0b910ed874a2ab
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-06-13 16:03:02 +0700

    Add WebSocket support for real-time notifications in Notifications component

--- src/Customer/Notification/Notifications.jsx
+++ src/Customer/Notification/Notifications.jsx
@@ -1,18 +1,54 @@
-import React, { useEffect } from 'react'
-import NotificationCard from './NotificationCard';
-import { useDispatch, useSelector } from 'react-redux';
-import {fetchNotificationsByUser} from "../../Redux/Notifications/action"
+import React, { useEffect, useState } from "react";
+import NotificationCard from "./NotificationCard";
+import { useDispatch, useSelector } from "react-redux";
+import { addNotification, fetchNotificationsByUser } from "../../Redux/Notifications/action";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
 
 const Notifications = () => {
-  const dispatch = useDispatch()
-  const {auth, notification} = useSelector(store => store)
+  const dispatch = useDispatch();
+  const { auth, notification } = useSelector((store) => store);
 
   useEffect(() => {
-    dispatch(fetchNotificationsByUser({
-      userId: auth.user?.id,
-      jwt: localStorage.getItem("jwt")
-    }));
-  },[])
+    if (auth.user?.id) {
+      dispatch(
+        fetchNotificationsByUser({
+          userId: auth.user?.id,
+          jwt: localStorage.getItem("jwt"),
+        }),
+      );
+    }
+  }, [auth.user?.id]);
+
+  // realtime notifications
+  const [stompClient, setStompClient] = useState(null);
+
+  useEffect(() => {
+    if (auth.user?.id) {
+      const sock = new SockJS("http://localhost:8000/api/v1/notifications/ws");
+      const stomp = Stomp.over(sock);
+      setStompClient(stomp);
+    }
+  }, [auth.user?.id]);
+
+  useEffect(() => {
+    if (stompClient) {
+      stompClient.connect(
+        {},
+        () => {
+          stompClient.subscribe(
+            `/notification/user/${auth.user?.id}`,
+            (message) => {
+              const receivedMessage = JSON.parse(message.body)
+              console.log("received notification from server: ", receivedMessage);
+              dispatch(addNotification(receivedMessage))
+            },
+          );
+        },
+        (error) => console.log("subcribe failed: ", error),
+      );
+    }
+  }, [stompClient, auth.user?.id]);
   return (
     <div className="px-5 md:flex flex-col items-center mt-10 min-h-screen">
       <div className="">
@@ -20,10 +56,12 @@ const Notifications = () => {
       </div>
 
       <div className="space-y-4 md:w-[35rem]">
-        {notification.notifications.map((item) => <NotificationCard item={item}/>)}
+        {notification.notifications.map((item) => (
+          <NotificationCard item={item} />
+        ))}
       </div>
     </div>
   );
-}
+};
 
-export default Notifications
+export default Notifications;
